cc52fea5af6971195511d1f08d3f70652238cc34
Alexander Klimov <alexander.klimov@genestack.com>
2020-10-28 11:04:53 +0300

    add db code for Heroku

--- db.py
+++ db.py
@@ -1,12 +1,24 @@
+import os
+from urllib.parse import urlparse
 from peewee import *
 
-db = PostgresqlDatabase(
-    'postgres',  # Required by Peewee.
-    user='aklimov',  # Will be passed directly to psycopg2.
-    # password='password',  # Ditto.
-    host='localhost',  # Ditto.
-    port=5432
-)
+if os.environ.get('DATABASE_URL'):
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    db = urlparse(DATABASE_URL)
+    user = db.username
+    password = db.password
+    path = db.path[1:]
+    host = db.hostname
+    port = db.port
+    db = PostgresqlDatabase(path, user=user, password=password, host=host, port=port)
+else:
+    db = PostgresqlDatabase(
+        'postgres',  # Required by Peewee.
+        user='aklimov',  # Will be passed directly to psycopg2.
+        # password='password',  # Ditto.
+        host='localhost',  # Ditto.
+        port=5432
+    )
 
 
 class News(Model):
